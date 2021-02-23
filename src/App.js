1dad4103c448e3ff66a47346b37fd0a92dd6146e
ESCORNEBOUEU <titouanescorneboueu@gmail.com>
2021-02-23 13:27:44 +0100

    Creation de mes components et de mes pages

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import scss from "./scss/style.scss";
+
 function App() {
   return <div className="App"></div>;
 }
---
+++ src/pages/Publication.js
@@ -0,0 +1,5 @@
+const Publication = () => {
+  return <div></div>;
+};
+
+export default Publication;
