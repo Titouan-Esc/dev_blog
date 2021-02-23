3b38e1fd59c7a301f3ad37ced40636c9f4144a42
ESCORNEBOUEU <titouanescorneboueu@gmail.com>
2021-02-23 13:39:33 +0100

    Fin du html pour la nav bar, place au scss de la nav bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import scss from "./scss/style.scss";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import React, { useState, useEffect } from "react";
-import React, { useState } from "react";
 import Home from "./pages/Home";
 
 function App() {
